e7e17bb47894f6043da4d2b07ecfa9c642c23759
johnwanjema <jonwanjema@gmail.com>
2019-06-21 12:40:44 +0300

    added class for articles

--- app/models.py
+++ app/models.py
@@ -3,11 +3,24 @@ class News:
     News class to define News Objects
     '''
 
-    def __init__(self,id,name,author):
+    def __init__(self,id,name,author,):
         self.id =id
         self.name = name
         self.author =author
-        self.tite = tite
+        self.title = title
+
+class Articles:
+    '''
+    News articles to define article Objects
+    '''
+    def __init__(self,name,title,description,url,urlToImage,publishesAt,content):
+        self.name = name
+        self.title = title  
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishesAt
+        self.content = content
         
 
 
