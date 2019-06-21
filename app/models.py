1471c38a7e73fbdde7c2eed6c6bcced088d8bb8d
johnwanjema <jonwanjema@gmail.com>
2019-06-21 12:34:02 +0300

    added class for news

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class News:
+    '''
+    News class to define News Objects
+    '''
+
+    def __init__(self,id,name,author):
+        self.id =id
+        self.name = name
+        self.author =author
+        self.tite = tite
+        
+
+
+
